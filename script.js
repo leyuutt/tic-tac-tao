b0f5dfbf40c90a63cd12be5a798252c1a69e5b48
Edlawit Tesfaye <edlawittesfaye73@gmail.com>
2026-05-01 16:50:32 +0300

    winning pattern

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ let scores = {
 };
 
 const winPatterns = [
-    [0, 1, 3], [1, 2, 4], [3, 4, 6], [4, 5, 7],
-    [0, 4, 5], [3, 4, 8], [1, 4, 6], [2, 4, 7],
-    [0, 2, 6], [2, 6, 8]
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
 ];
 
 const modeSelection = document.getElementById('modeSelection');
